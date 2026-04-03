62e3ee7e28b4c87d33ed0a443df54fa91c66d833
Ario Moniri <92126657+ArioMoniri@users.noreply.github.com>
2026-04-03 19:12:54 +0300

    fix: export new clients from __init__.py

--- src/variant_mcp/clients/__init__.py
+++ src/variant_mcp/clients/__init__.py
@@ -1,9 +1,11 @@
 """API clients for external genomic databases."""
 
 from .base_client import BaseClient
+from .cancer_hotspots_client import CancerHotspotsClient
 from .civic_client import CIViCClient
 from .clinvar_client import ClinVarClient
 from .gnomad_client import GnomADClient
+from .litvar_client import LitVarClient
 from .metakb_client import MetaKBClient
 from .myvariant_client import MyVariantClient
 from .oncokb_client import OncoKBClient
@@ -12,9 +14,11 @@ from .uniprot_client import UniProtClient
 
 __all__ = [
     "BaseClient",
+    "CancerHotspotsClient",
     "CIViCClient",
     "ClinVarClient",
     "GnomADClient",
+    "LitVarClient",
     "MetaKBClient",
     "MyVariantClient",
     "OncoKBClient",
